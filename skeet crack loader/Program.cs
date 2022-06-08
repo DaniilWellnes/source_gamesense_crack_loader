c818bf60930032f5a5164c0e437fe841eb13c61d
DaniilStarostin <72040412+DaniilStarostin@users.noreply.github.com>
2022-06-09 02:12:04 +0300

    1

---
+++ skeet crack loader/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace gamesense_crack
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new load());
+        }
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+    }
+}
